7527314285cbd9a0bf5f7f4968dc82607f447b57
swandyr <swich86@free.fr>
2023-03-10 14:25:44 +0100

    match pattern

---
+++ clearurl/README.md
@@ -0,0 +1,5 @@
+# ClearUrl lib for brztek
+
+Use rules from the ClearUrls addon: [https://github.com/ClearURLs/Addon]
+
+(rules)[https://rules2.clearurls.xyz/data.minify.json]
--- clearurl/src/lib.rs
+++ clearurl/src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use regex::Regex;
+
+const RULES: &str = "https://rules2.clearurls.xyz/data.minify.json";
+
+pub async fn clear_url(url: &str) -> Result<&str, Box<dyn std::error::Error>> {
+    let rules = reqwest::get(RULES).await?.text().await?;
+    let rules: serde_json::Value = serde_json::from_str(&rules)?;
+
+    let providers = rules["providers"].clone();
+    let providers = providers.as_object().unwrap();
+
+    for provider in providers {
+        let name = provider.0;
+        let data = provider.1;
+
+        let url_pattern = data
+            .get("urlPattern")
+            .unwrap()
+            .as_str()
+            .unwrap()
+            .replace("\\", "");
+        let re = Regex::new(&url_pattern)?;
+
+        if re.is_match(url) {
+            println!("found match: {name} - {}", re.as_str());
+        }
+    }
+
+    Ok(url)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[tokio::test]
+    async fn it_works() {
+        let url = "https://www.amazon.com/dp/exampleProduct/ref=sxin_0_pb?__mk_de_DE=ÅMÅŽÕÑ&keywords=tea&pd_rd_i=exampleProduct&pd_rd_r=8d39e4cd-1e4f-43db-b6e7-72e969a84aa5&pd_rd_w=1pcKM&pd_rd_wg=hYrNl&pf_rd_p=50bbfd25-5ef7-41a2-68d6-74d854b30e30&pf_rd_r=0GMWD0YYKA7XFGX55ADP&qid=1517757263&rnid=2914120011";
+        let cleared = "https://www.amazon.com/dp/exampleProduct";
+
+        match clear_url(url).await {
+            Ok(val) => assert_eq!(val, cleared),
+            Err(e) => println!("Error: {e}"),
+        }
     }
 }
